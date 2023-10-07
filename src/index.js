260d5306354a8b567655ab60c2c20eb9dfa244a2
Vartanush <anna87-07@inbox.ru>
2023-10-07 14:52:38 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux/es/exports';
+
+import App from './components/app/App';
+import store from './store/store';
+
+import './styles/index.scss';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Provider store ={store}>
+        <App/>
+    </Provider>
+    
+);
+
+
